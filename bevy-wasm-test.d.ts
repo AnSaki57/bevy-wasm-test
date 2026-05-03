7a6faf630f6a0e371978fc32b8b563de74ece3d2
AnSaki57 <AnSaki57@users.noreply.github.com>
2026-05-03 07:06:19 +0000

    deploy: a2b2b5a71dc517014c1f126291a84a6e76991b38

---
+++ bevy-wasm-test.d.ts
@@ -0,0 +1,53 @@
+/* tslint:disable */
+/* eslint-disable */
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+    readonly memory: WebAssembly.Memory;
+    readonly main: (a: number, b: number) => number;
+    readonly wasm_bindgen__convert__closures_____invoke__h7c5bd01d570a6738: (a: number, b: number, c: any) => [number, number];
+    readonly wasm_bindgen__convert__closures_____invoke__h068f7d32c024fc39: (a: number, b: number, c: any, d: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h0bc9ed3d47240eda: (a: number, b: number, c: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h0bc9ed3d47240eda_3: (a: number, b: number, c: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h0bc9ed3d47240eda_4: (a: number, b: number, c: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h0bc9ed3d47240eda_5: (a: number, b: number, c: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h0bc9ed3d47240eda_6: (a: number, b: number, c: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h0bc9ed3d47240eda_7: (a: number, b: number, c: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h0bc9ed3d47240eda_8: (a: number, b: number, c: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h0bc9ed3d47240eda_9: (a: number, b: number, c: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h3a3f75672ae77812: (a: number, b: number, c: number) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h9f025e662aa15763: (a: number, b: number) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h1c9329db12bbc58f: (a: number, b: number) => void;
+    readonly __wbindgen_malloc: (a: number, b: number) => number;
+    readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+    readonly __externref_table_alloc: () => number;
+    readonly __wbindgen_externrefs: WebAssembly.Table;
+    readonly __wbindgen_exn_store: (a: number) => void;
+    readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+    readonly __wbindgen_destroy_closure: (a: number, b: number) => void;
+    readonly __externref_table_dealloc: (a: number) => void;
+    readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+
+/**
+ * Instantiates the given `module`, which can either be bytes or
+ * a precompiled `WebAssembly.Module`.
+ *
+ * @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+ *
+ * @returns {InitOutput}
+ */
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+ * If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+ * for everything else, calls `WebAssembly.instantiate` directly.
+ *
+ * @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+ *
+ * @returns {Promise<InitOutput>}
+ */
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ bevy-wasm-test_bg.wasm.d.ts
@@ -0,0 +1,26 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: (a: number, b: number) => number;
+export const wasm_bindgen__convert__closures_____invoke__h7c5bd01d570a6738: (a: number, b: number, c: any) => [number, number];
+export const wasm_bindgen__convert__closures_____invoke__h068f7d32c024fc39: (a: number, b: number, c: any, d: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h0bc9ed3d47240eda: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h0bc9ed3d47240eda_3: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h0bc9ed3d47240eda_4: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h0bc9ed3d47240eda_5: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h0bc9ed3d47240eda_6: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h0bc9ed3d47240eda_7: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h0bc9ed3d47240eda_8: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h0bc9ed3d47240eda_9: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h3a3f75672ae77812: (a: number, b: number, c: number) => void;
+export const wasm_bindgen__convert__closures_____invoke__h9f025e662aa15763: (a: number, b: number) => void;
+export const wasm_bindgen__convert__closures_____invoke__h1c9329db12bbc58f: (a: number, b: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_destroy_closure: (a: number, b: number) => void;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
